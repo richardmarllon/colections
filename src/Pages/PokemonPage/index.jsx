9e0b3e7a592abfdf3c4f271822ce44e80af67a0c
richard <richard_marllon@hotmail.com>
2021-02-27 21:57:22 -0300

    pokemon page working

--- src/Pages/PokemonPage/index.jsx
+++ src/Pages/PokemonPage/index.jsx
@@ -1,16 +1,26 @@
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import CardList from "../../components/CardList/CardList";
 
 const PokemonPage = () => {
-  const [pokemons, setPokemons] = useState();
-
+  const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
+  );
   useEffect(() => {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?offset=20&limit=20")
-      .then((response) => setPokemons(response.data.results));
-  }, []);
+      .get(page)
+      .then((response) => setPokemons(response.data.results))
+      .catch((err) => console.log(err, "error in axios"));
+  }, [page]);
+
   console.log(pokemons);
-  return <div></div>;
+
+  return (
+    <div>
+      <CardList list={pokemons}></CardList>
+    </div>
+  );
 };
 export default PokemonPage;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,33 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import StyledButton from "../StyledButton/StyledButton";
 import { CardContainer, GenericText, ImgBox, InfoBox, NameBox } from "./style";
 
 const Card = ({ item }) => {
   // console.log(item, "no card");
+  const [pokemon, setPokemon] = useState([]);
+  const [urlImg, setUrlImg] = useState("");
+
+  useEffect(() => {
+    // console.log(item, " item");
+    if (!item.id) {
+      axios
+        .get(item.url)
+        .then((response) => {
+          setPokemon(response.data);
+          setUrlImg(
+            response.data.sprites.other["official-artwork"].front_default
+          );
+        })
+        .catch((error) => console.log(error, "erro no catch"));
+    }
+  }, []);
+
   return (
     <CardContainer>
-      <ImgBox src={item.image}></ImgBox>
+      {item.image && <ImgBox src={item.image}></ImgBox>}
+      {urlImg && <ImgBox src={urlImg}></ImgBox>}
       <InfoBox>
         <NameBox>{item.name}</NameBox>
         {item.species && <GenericText>{item.species}</GenericText>}
